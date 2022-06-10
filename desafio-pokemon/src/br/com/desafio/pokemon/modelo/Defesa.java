ed05ad836fcb436fa51f7b18e6901a3cd874e21e
Marcela Freitas <93737186+mff7@users.noreply.github.com>
2022-06-10 16:15:08 -0300

    iniciando o desafio

---
+++ desafio-pokemon/src/br/com/desafio/pokemon/modelo/Defesa.java
@@ -0,0 +1,5 @@
+package br.com.desafio.pokemon.modelo;
+
+public class Defesa {
+
+}
---
+++ desafio-pokemon/src/br/com/desafio/pokemon/modelo/Pokemon.java
@@ -0,0 +1,10 @@
+package br.com.desafio.pokemon.modelo;
+
+public class Pokemon {
+
+	private int vida;
+	private String nome;
+	private Tipo tipo;
+	private Ataque ataque;
+	private Defesa Defesa;
+}
