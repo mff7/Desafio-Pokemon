cf3586e29fe57bffea3aa757181f6eb5155e1c3b
Marcela Freitas <93737186+mff7@users.noreply.github.com>
2022-06-17 20:48:46 -0300

    Update Ataque.java

--- desafio-pokemon/src/br/com/desafio/pokemon/modelo/Ataque.java
+++ desafio-pokemon/src/br/com/desafio/pokemon/modelo/Ataque.java
@@ -1,5 +1,34 @@
+// PACOTE
 package br.com.desafio.pokemon.modelo;
 
 public class Ataque {
 
+// ATRIBUTO
+
+	private int ataqueBase;
+
+// GET
+
+	public int getValorAtaque1() {
+		return this.ataqueBase;
+	}
+
+	public int getValorAtaque2() {
+		return this.ataqueBase + 5;
+	}
+
+	public int getValorAtaque3() {
+		return this.ataqueBase * 2;
+	}
+
+	public int getValorAtaque4() {
+		return this.ataqueBase + 3;
+	}
+
+// SET
+
+	public void setValorAtaqueBase(int valorAtaqueBase) {
+		this.ataqueBase = valorAtaqueBase;
+	}
 }
+
