23f1f97f64b502afad8c5e0bd7aae0bac742e4eb
Marcela Freitas <93737186+mff7@users.noreply.github.com>
2022-06-17 20:49:44 -0300

    Update Tipo.java

--- desafio-pokemon/src/br/com/desafio/pokemon/modelo/Tipo.java
+++ desafio-pokemon/src/br/com/desafio/pokemon/modelo/Tipo.java
@@ -1,5 +1,41 @@
+//PACOTE
 package br.com.desafio.pokemon.modelo;
 
 public class Tipo {
 
+//ATRIBUTOS
+
+	private int idTipo;
+	private String tituloTipo;
+
+//CONSTRUTOR
+
+	public Tipo(int idTipo) {
+		if (idTipo == 1) {
+			this.tituloTipo = "Agua";
+		} else if (idTipo == 2) {
+			this.tituloTipo = "Fogo";
+		} else if (idTipo == 3) {
+			tituloTipo = "Terra";
+		} else if (idTipo == 4) {
+			tituloTipo = "Ar";
+		}
+	}
+
+//GET
+
+	public int getIdTipo() {
+		return idTipo;
+	}
+
+	public String getTipo() {
+
+		return tituloTipo;
+	}
+
+//SET
+	
+	public void setIdtipo(int idTipo) {
+		this.idTipo = idTipo;
+	}
 }
