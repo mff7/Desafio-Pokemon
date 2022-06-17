9c042091e4026f12bf31e135f1e3f426bebecc4c
Marcela Freitas <93737186+mff7@users.noreply.github.com>
2022-06-17 20:48:06 -0300

    Update Defesa.java

--- desafio-pokemon/src/br/com/desafio/pokemon/modelo/Defesa.java
+++ desafio-pokemon/src/br/com/desafio/pokemon/modelo/Defesa.java
@@ -1,5 +1,34 @@
+//PACOTE
 package br.com.desafio.pokemon.modelo;
 
 public class Defesa {
 
+// ATRIBUTO
+
+	private int defesaBase;
+
+// GET
+
+	public int getValorDefesa1() {
+		return defesaBase;
+	}
+
+	public int getValorDefesa2() {
+		return defesaBase * 2;
+	}
+
+	public int getValorDefesa3() {
+		return defesaBase + 7;
+
+	}
+
+	public int getValorDefesa4() {
+		return defesaBase + 15;
+
+	}
+
+// SET
+	public void setValorDefesaBase(int valorDefesa) {
+		this.defesaBase = valorDefesa;
+	}
 }
